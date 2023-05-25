488c56c38bf7403973ff8090341b3a1aa2f6a505
clementbtt <cboutetdrumsgo10@gmail.com>
2023-05-25 10:20:17 +0200

    nouvelle branche

--- TavernManager/obj/Debug/net6.0-windows/App.g.i.cs
+++ TavernManager/obj/Debug/net6.0-windows/App.g.i.cs
@@ -47,7 +47,7 @@ namespace TavernManager {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.5.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.2.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
@@ -59,7 +59,7 @@ namespace TavernManager {
             
             #line default
             #line hidden
-            System.Uri resourceLocater = new System.Uri("/TavernManager;V1.0.0.0;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/TavernManager;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -73,7 +73,7 @@ namespace TavernManager {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.5.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.2.0")]
         public static void Main() {
             SplashScreen splashScreen = new SplashScreen("vues/ressources/tavernmanager.jpg");
             splashScreen.Show(true);
--- TavernManager/obj/Debug/net6.0-windows/Vues/FenetrePrincipale.g.i.cs
+++ TavernManager/obj/Debug/net6.0-windows/Vues/FenetrePrincipale.g.i.cs
@@ -127,13 +127,13 @@ namespace TavernManager.Vues {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.5.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.2.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/TavernManager;V1.0.0.0;component/vues/fenetreprincipale.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/TavernManager;component/vues/fenetreprincipale.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Vues\FenetrePrincipale.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -143,14 +143,14 @@ namespace TavernManager.Vues {
         }
         
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.5.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.2.0")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
         internal System.Delegate _CreateDelegate(System.Type delegateType, string handler) {
             return System.Delegate.CreateDelegate(delegateType, this, handler);
         }
         
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.5.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.2.0")]
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
